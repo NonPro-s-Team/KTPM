9786ad403ece95891ca728677de8e3d25de9a67d
hi3nt <hientm0978@gmail.com>
2026-07-30 14:27:58 +0700

    feat(application): add invoice and payment use cases

--- backend/src/RMS.Application/Features/Invoices/Dtos/CreateInvoiceRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace RMS.Application.Features.Invoices.Dtos;
-
-public class CreateInvoiceRequest
-{
-    public Guid ContractId { get; set; }
-    public int BillingMonth { get; set; }
-    public int BillingYear { get; set; }
-}
--- backend/src/RMS.Application/Features/Invoices/IInvoiceService.cs
+++
@@ -1,3 +0,0 @@
-namespace RMS.Application.Features.Invoices;
-
-public interface IInvoiceService { }
