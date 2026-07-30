1d2cff88ab3ac48406b4cf78b6c56128cbdb050e
hi3nt <hientm0978@gmail.com>
2026-07-30 23:14:48 +0700

    merge br#6

--- backend/src/RMS.API/obj/Debug/net10.0/RMS.API.AssemblyInfo.cs
+++ backend/src/RMS.API/obj/Debug/net10.0/RMS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69ff8623f9cb6bc08ae6b1a92699285d0352d05c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05b9c1767c6482e1eeb3cea68b01b0be47e24fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("RMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
